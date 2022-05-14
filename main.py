218d870492410f5ee856f50b013a0d89db59c55b
Engmelli <105137053+Engmelli@users.noreply.github.com>
2022-05-14 16:16:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import shutil
 from tkinter import *
 from tkinter import filedialog
+from tkinter.ttk import Progressbar
 from openpyxl import load_workbook
 import os
+import tika
+tika.initVM()
 from tika import parser
 import pyperclip
+import math
 
 wb = load_workbook("EXCEL.xlsx")
 
@@ -16,6 +20,25 @@ def filesget():
     files = file
 
 def submit():
+
+    button1.config(state=DISABLED)
+    button2.config(state=DISABLED)
+
+    progressbar.pack(pady=30, padx=30)
+    global completedtasks, tasks
+    tasks = len(files)
+    taskvalue = 100 / tasks
+    completedtasks = 0
+    taskcounter.set(str(completedtasks) + "/" +str(tasks)+ " tasks completed")
+    taskcount.pack()
+
+    def progress():
+        global completedtasks, tasks
+        progressbar['value'] += taskvalue
+        completedtasks += 1
+        taskcounter.set(str(completedtasks) + " out of " + str(tasks) + " tasks completed")
+        window.update_idletasks()
+
 
     for file in files:
         raw = parser.from_file(file)
@@ -46,11 +69,11 @@ def submit():
         accountdesc = filedata[filedata.index('Summary') + 3]
         readingdesc = " بقراءة " + filedata[filedata.index('Capacity:') + 5][:-16] + " "
         averagecons = int(filedata[filedata.index('Quantity:') + 1][:-13]) / int(filedata[filedata.index('Class') + 5][6:8])
-        average = "ومعدل استهلاك " + str(int(averagecons)) + " "
+        average = "ومعدل استهلاك " + str(int(math.ceil(averagecons))) + " "
         extratext = "في اليوم من تاريخ "
 
         if filedata[filedata.index('Summary') + 3] == '10054716660':
-            path = "C:\\Users\\LENOVO\\Downloads\\42"
+            path = "D:\\Downloads\\42"
             filename = filedata[filedata.index('Class') + 3][5:12]
             filename = filename.replace(filename[filename.index("/")], "-") + ".pdf"
 
@@ -69,8 +92,10 @@ def submit():
 
             descs.append(finalDesc)
 
+            progress()
+
         elif filedata[filedata.index('Summary') + 3] == '10062328912':
-            path = "C:\\Users\\LENOVO\\Downloads\\5"
+            path = "D:\\Downloads\\5"
             filename = filedata[filedata.index('Class') + 3][5:12]
             filename = filename.replace(filename[filename.index("/")], "-") + ".pdf"
 
@@ -89,8 +114,10 @@ def submit():
 
             descs.append(finalDesc)
 
+            progress()
+
         elif filedata[filedata.index('Summary') + 3] == '30020596395':
-            path = "C:\\Users\\LENOVO\\Downloads\\11"
+            path = "D:\\Downloads\\11"
             filename = filedata[filedata.index('Class') + 3][5:12]
             filename = filename.replace(filename[filename.index("/")], "-") + ".pdf"
 
@@ -109,8 +136,10 @@ def submit():
 
             descs.append(finalDesc)
 
+            progress()
+
         elif filedata[filedata.index('Summary') + 3] == '30013775583':
-            path = "C:\\Users\\LENOVO\\Downloads\\27"
+            path = "D:\\Downloads\\27"
             filename = filedata[filedata.index('Class') + 3][5:12]
             filename = filename.replace(filename[filename.index("/")], "-") + ".pdf"
 
@@ -129,8 +158,10 @@ def submit():
 
             descs.append(finalDesc)
 
+            progress()
+
         elif filedata[filedata.index('Summary') + 3] == '10054624906':
-            path = "C:\\Users\\LENOVO\\Downloads\\34"
+            path = "D:\\Downloads\\34"
             filename = filedata[filedata.index('Class') + 3][5:12]
             filename = filename.replace(filename[filename.index("/")], "-") + ".pdf"
 
@@ -149,8 +180,10 @@ def submit():
 
             descs.append(finalDesc)
 
+            progress()
+
         elif filedata[filedata.index('Summary') + 3] == '10054717729':
-            path = "C:\\Users\\LENOVO\\Downloads\\44"
+            path = "D:\\Downloads\\44"
             filename = filedata[filedata.index('Class') + 3][5:12]
             filename = filename.replace(filename[filename.index("/")], "-") + ".pdf"
 
@@ -169,6 +202,8 @@ def submit():
 
             descs.append(finalDesc)
 
+            progress()
+
         else:
             pass
 
@@ -178,16 +213,15 @@ def submit():
     data_string3 = StringVar()
     data_string3.set(descs)
     listbox = Listbox(frame2, listvariable=data_string3, font = ("Arial", 10), bd= 0, width= 100, fg= "black", borderwidth=6, relief="solid", justify= CENTER)
-    listbox.pack(pady=10)
+    listbox.pack(pady=10, padx= 10)
 
     def printt():
         currentselect = listbox.get(ANCHOR)
         pyperclip.copy(currentselect)
 
-    button3 = Button(frame2, command=printt, text="Copy", font = ("Arial", 20), bd= 0, fg= "black").pack()
+    button3 = Button(frame2, command=printt, text="Copy", font = ("Arial", 20), bd= 0, fg= "black", background= "white").pack()
 
-    button1.config(state=DISABLED)
-    button2.config(state=DISABLED)
+
 
 
 
@@ -208,18 +242,22 @@ frameMain = Frame(window).pack()
 data_string = StringVar()
 data_string2 = StringVar()
 data_string.set("30011715675 - 30020596395 - 30013775583")
-entry = Entry(frameMain, state= "readonly", textvariable= data_string, width= 37, font = ("Arial", 20), bd= 0, fg= "black").pack()
+entry = Entry(frameMain, state= "readonly", textvariable= data_string, width= 37, font = ("Arial", 20), bd= 0, fg= "black", background= "white").pack()
 data_string2.set("30010522932 - 30010522923 - 30029501703")
-entry2 = Entry(frameMain, state= "readonly", textvariable= data_string2, width= 37, font = ("Arial", 20), bd= 0, fg= "black").pack()
+entry2 = Entry(frameMain, state= "readonly", textvariable= data_string2, width= 37, font = ("Arial", 20), bd= 0, fg= "black", background= "white").pack()
 
 button1 = Button(frameMain, text= "Attach files", background= "white", font= ("Arial", 20), command= filesget)
 button1.pack(pady= 20)
 button2 = Button(frameMain, text= "Submit", background= "white", font= ("Arial", 20), command= submit)
 button2.pack()
 
+progressbar = Progressbar(window, orient=HORIZONTAL, length=300)
+taskcounter = StringVar()
+taskcount= Label(window, textvariable= taskcounter, background= "white", font = ("Arial", 15))
+
+
 
 
 
 
 window.mainloop()
-
